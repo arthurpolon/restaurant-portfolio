905bcc3d2376b407abd122e4f96f15d96d5331ee
Arthur Polon Lemos <arthurpolon@hotmail.com>
2022-11-08 10:10:34 -0300

    📦 NEW: add colors variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ff9F0D',
+        secondary: '#999966',
+        info: '#2F80ED',
+        success: '#27AE60',
+        warning: '#E2B93B',
+        error: '#EB5757',
+        'black-1': '#1E1E1E',
+        'black-2': '#1D1D1D',
+        'black-3': '#282828',
+        'gray-1': '#333333',
+        'gray-2': '#4F4F4F',
+        'gray-3': '#828282',
+        'gray-4': '#BDBDBD',
+        'gray-5': '#E0E0E0',
+      },
+    },
   },
   plugins: [],
 };
