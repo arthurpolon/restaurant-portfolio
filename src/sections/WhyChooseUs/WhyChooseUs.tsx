aa305d5178cf4b1bab10446f23d7e90cd55ce300
Mateus Faria <mateusfariadomingos@hotmail.com>
2022-11-21 00:21:43 -0400

    feat: create section why choose us

---
+++ src/sections/WhyChooseUs/WhyChooseUs.tsx
@@ -0,0 +1,27 @@
+const WhyChooseUs = () => {
+  return (
+    <div className="bg-black-1">
+      <div className="section-content py-14 flex justify-between">
+        <div className="flex flex-col"></div>
+        <div className="flex flex-col">
+          <h2 className="font-fancy text-primary text-3xl mb-2">
+            Why Choose Us{' '}
+          </h2>
+          <h3 className="font-bold text-4xl text-white mb-8">
+            <span className="text-primary">Ex</span>traordinay taste
+            <br /> And Experienced
+          </h3>
+          <p className="text-base text-white max-w-lg mb-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+            diam pellentesque bibendum non dui volutpat fringilla bibendum.
+            Urna, elit augue urna, vitae feugiat pretium donec id elementum.
+            Ultrices mattis sed vitae mus risus. Lacus nisi, et ac dapibus sit
+            eu velit in consequat.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WhyChooseUs;
